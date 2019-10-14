f67c6f055af435d7b76c609c31c5cd920562ef33
Philipp Richter <pr@cce-factory.de>
2019-10-14 19:41:41 +0200

    project1

---
+++ cs50x_Python_Js/project1/application.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask, session, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home")
+
+@app.route("/login")
+def login():
+    return render_template("login.html",title="Login")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html", title="Logout")
---
+++ cs50x_Python_Js/project1/models.py
@@ -0,0 +1,3 @@
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+
